9b5d287bd5cea99925cec05d18da11187912a2ec
Jeremiah Heller <jeremiah@impactdialing.com>
2016-02-01 12:07:57 -0800

    add designated 'busy' number

--- lib/dials.rb
+++ lib/dials.rb
@@ -1,10 +1,21 @@
 require 'csv'
 
 class Dials
+  def self.busy_phone
+    '5551237890'
+  end
+
+  def self.busy_sid
+    'CA33F5C5261BD94898A8680074701AB674'
+  end
+
   def self.data
     return @data if defined?(@data)
 
-    data = {}
+    data = {
+      busy_phone => busy_sid
+    }
+
     path = File.join(File.dirname(__FILE__), '..', 'data')
     glob = File.join path, 'dials*.csv'
     Dir.glob(glob).each do |filename|
